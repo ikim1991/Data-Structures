e9eeb83e153349db34afe50ce4f677b683f3fb0a
Chris K <ikim1991@gmail.com>
2020-03-27 05:28:50 -0400

    Initialize Dynamic Programming

---
+++ Dynamic Programming/README.md
@@ -0,0 +1,3 @@
+# Dynamic Programming  
+
+### What is Dynamic Programming?  
---
+++ Dynamic Programming/src/algorithms/dynamicprogramming/Main.java
@@ -0,0 +1,8 @@
+package algorithms.dynamicprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    
+    }
+}
